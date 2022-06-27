c9e42745fd0489c0569c6c1c4ea39615f9df050c
AndreaValdez13 <47536477+AndreaValdez13@users.noreply.github.com>
2022-06-26 19:34:20 -0600

    Add files via upload

---
+++ bd_proyecto.sql
@@ -0,0 +1,305 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-06-2022 a las 03:30:50
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_proyecto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Mineria'),
+(2, 'Materia Prima'),
+(3, 'Maquinaria de Construcción'),
+(4, 'Transportes');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `kind`
+--
+
+CREATE TABLE `kind` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `kind`
+--
+
+INSERT INTO `kind` (`id`, `name`) VALUES
+(1, 'Alquiler'),
+(2, 'Viaje'),
+(3, 'Proceso');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `priority`
+--
+
+CREATE TABLE `priority` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `priority`
+--
+
+INSERT INTO `priority` (`id`, `name`) VALUES
+(1, 'Nacional'),
+(2, 'Extrajero');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `project`
+--
+
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `project`
+--
+
+INSERT INTO `project` (`id`, `name`, `description`) VALUES
+(4, 'Planta de extracción Cobán', 'Planta de extracción de mineria'),
+(5, 'Planta de extracción El Progreso', 'Planta de extracción de mineria'),
+(6, 'Planta de extracción Quetzaltenango', 'Planta de extracción de mineria'),
+(7, 'Planta de extracción Peten', 'Planta de extracción de mineria'),
+(8, 'Planta de Proceso Zacapa', 'Planta de proceso de materia prima'),
+(9, 'Planta de Proceso Huehuetenango', 'Planta de Proceso de materia prima'),
+(10, 'Sucursal Guatemala', 'Alquiler y transporte de materia prima.'),
+(11, 'Sucursal Xela', 'Alquiler y transporte de materia prima.'),
+(12, 'Sucursal Zacapa', 'Alquiler y transporte de materia prima.'),
+(13, 'Sucursal Peten', 'Alquiler y transporte de materia prima.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `status`
+--
+
+INSERT INTO `status` (`id`, `name`) VALUES
+(1, 'Finalizado'),
+(2, 'En curso'),
+(3, 'Cancelado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `kind_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `asigned_id` int(11) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `priority_id` int(11) NOT NULL DEFAULT 1,
+  `status_id` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `ticket`
+--
+
+INSERT INTO `ticket` (`id`, `title`, `description`, `updated_at`, `created_at`, `kind_id`, `user_id`, `asigned_id`, `project_id`, `category_id`, `priority_id`, `status_id`) VALUES
+(13, 'Alquiler de Maquinaria', 'Se alquilo maquinaria a un cliente comercial', NULL, '2022-06-25 15:56:21', 1, 2, NULL, 11, 3, 1, 2),
+(14, 'Proceso de Hierro', 'Actualmente se esta realizando la extracción de hierro. ', NULL, '2022-06-25 16:00:57', 3, 21, NULL, 4, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `profile_pic` varchar(250) DEFAULT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `kind` int(11) NOT NULL DEFAULT 1,
+  `created_at` datetime DEFAULT NULL,
+  `rol` tinyint(1) NOT NULL,
+  `codempleado` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `name`, `email`, `password`, `profile_pic`, `is_active`, `kind`, `created_at`, `rol`, `codempleado`) VALUES
+(2, 'Andrea', 'Andrea Valdez', 'andrea@correo.com', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', 'profile2.png', 1, 1, '2022-06-25 12:03:00', 1, 0),
+(21, NULL, 'Sofia Vasquez', 'sofia@correo.com', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', 'profile2.png', 1, 1, '2022-06-25 23:58:44', 0, 3);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `kind`
+--
+ALTER TABLE `kind`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `priority`
+--
+ALTER TABLE `priority`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `priority_id` (`priority_id`),
+  ADD KEY `status_id` (`status_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `kind_id` (`kind_id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `project_id` (`project_id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `kind`
+--
+ALTER TABLE `kind`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `priority`
+--
+ALTER TABLE `priority`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `project`
+--
+ALTER TABLE `project`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`priority_id`) REFERENCES `priority` (`id`),
+  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`),
+  ADD CONSTRAINT `ticket_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `ticket_ibfk_4` FOREIGN KEY (`kind_id`) REFERENCES `kind` (`id`),
+  ADD CONSTRAINT `ticket_ibfk_5` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  ADD CONSTRAINT `ticket_ibfk_6` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
